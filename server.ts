fa6f53123f2cb65ed4702bf3506892efcbad6ca5
aggelos tranidis <aggelos@athletopia.com>
2024-02-22 18:17:15 +0200

    path changes

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
-import customerRoutes from '../Back-end/src/routes/customer.routes';
-import productRoutes from '../Back-end/src/routes/product.routes';
-import orderRoutes from '../Back-end/src/routes/order.routes';
+import customerRoutes from './src/routes/customer.routes';
+import productRoutes from './src/routes/product.routes';
+import orderRoutes from './src/routes/order.routes';
 import cors from 'cors';
 
 
