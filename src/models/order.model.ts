237f0d1515bdcf3270d80835cce7caf9d7ed13dc
aggelos tranidis <aggelos@athletopia.com>
2024-02-27 13:33:30 +0200

    commit orderschema

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -4,12 +4,14 @@ export interface Order extends Document {
   products: string; 
   customer: string; 
   orderDate: Date;
+  customerName: string;
 }
 
 const orderSchema: Schema = new Schema({
   products: [{ type: String }],
   customer: { type: String , required: true },
   orderDate: { type: Date, default: Date.now },
+  customerName: { type: String }
 
 });
 
